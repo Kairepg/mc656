781d02aaf3842a9f23af402991d40544dc69a62c
Ludivik de Paula <l235075@dac.unicamp.br>
2024-09-06 11:00:09 -0300

    add Firebase base structure

--- flutter_application_mc656/lib/main.dart
+++ flutter_application_mc656/lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_mc656/pages/home/home_page.dart';
 import 'package:flutter_application_mc656/routes/routes.dart';
+import 'package:flutter_application_mc656/services/firebase_options.dart';
 import 'package:flutter_application_mc656/utils/constants.dart';
 
-void main() {
+Future<void> main() async {
+  // firebase initialization
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // run the app
   runApp(const MyApp());
 }
 
