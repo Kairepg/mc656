c5e0d1ac755486a13e4f4cba486a9481f3abcc71
Guilherme <g260469@dac.unicamp.br>
2024-10-02 23:46:49 -0300

    streambuilder

--- fitness_buddy/lib/pages/profile/account_info/account_info_page.dart
+++ fitness_buddy/lib/pages/profile/account_info/account_info_page.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fitness_buddy/pages/profile/account_info/account_info_view.dart';
 import 'package:fitness_buddy/pages/profile/menu/menu_navigator_page.dart';
 import 'package:fitness_buddy/pages/profile/menu/menu_navigator_view.dart';
+import 'package:fitness_buddy/widgets/snackbars.dart';
 import 'package:flutter/material.dart';
 
 class AccountInfoView extends MenuNavigatorPage {
@@ -15,67 +16,84 @@ class AccountInfoView extends MenuNavigatorPage {
 class _AccountInfoViewState extends State<AccountInfoView> {
   bool _isHidden = false;
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  String emailId = '';
+  String passwordDisplay = "";
 
   void _togglePasswordView() {
     setState(() {
       _isHidden = !_isHidden;
     });
   }
-  
+
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
     final user = _auth.currentUser;
-    String emailId = '';
-
     if (user != null) {
       emailId = user.email ?? '';
     }
+    super.initState();
+  }
 
-    Widget emailDisplay = Text(emailId);
-    Widget passwordDisplay = getUserPassword(emailId);
+  @override
+  Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> usersStream =
+        FirebaseFirestore.instance.collection('users').snapshots();
 
-    Widget visibility = IconButton(
-      onPressed: _togglePasswordView,
-      icon: Icon(
-        _isHidden
-            ?
 
-            /// CHeck Show & Hide.
-            Icons.visibility
-            : Icons.visibility_off,
-      ),
-    );
+    return StreamBuilder(
+        stream: usersStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          
+          Widget emailDisplay = Text(emailId);
+          Widget visibility = IconButton(
+            onPressed: _togglePasswordView,
+            icon: Icon(
+              _isHidden ? Icons.visibility : Icons.visibility_off,
+            ),
+          );
 
-    return Column(children: [
-      DisplayCard(
-        text: emailDisplay,
-        subtitle: "Email:",
-        showIcon: false,
-      ),
-      DisplayCard(
-          text: _isHidden ? passwordDisplay : const Text("********"),
-          subtitle: "Senha:",
-          showIcon: true,
-          icon: visibility),
-      const SizedBox(height: 30),
-      BtnFilled(
-        text: "Alterar dados",
-        onPressed: () {
-          Navigator.pushNamed(context, '/accountInfoChange');
-        },
-        backgroundColor: Theme.of(context).primaryColor,
-        textColor: Colors.white,
-      ),
-      const SizedBox(height: 30),
-      BtnFilled(
-          text: "Voltar",
-          onPressed: () {
-            Navigator.pushNamed(context, '/');
-          },
-          backgroundColor: Colors.white,
-          textColor: Theme.of(context).primaryColor)
-    ]);
+          final userDoc =
+              snapshot.data!.docs.firstWhere((doc) => doc.id == emailId);
+          
+          final data = userDoc.data() as Map<String, dynamic>;
+
+
+          if (snapshot.hasData) {
+            return Column(children: [
+              DisplayCard(
+                text: emailDisplay,
+                subtitle: "Email:",
+                showIcon: false,
+              ),
+              DisplayCard(
+                  text: _isHidden
+                      ? Text(data['password'])
+                      : const Text("********"),
+                  subtitle: "Senha:",
+                  showIcon: true,
+                  icon: visibility),
+              const SizedBox(height: 30),
+              BtnFilled(
+                text: "Alterar dados",
+                onPressed: () {
+                  Navigator.pushNamed(context, '/accountInfoChange');
+                },
+                backgroundColor: Theme.of(context).primaryColor,
+                textColor: Colors.white,
+              ),
+              const SizedBox(height: 30),
+              BtnFilled(
+                  text: "Voltar",
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/');
+                  },
+                  backgroundColor: Colors.white,
+                  textColor: Theme.of(context).primaryColor)
+            ]);
+          }
+          return const Text("Loading");
+        });
   }
 }
 
@@ -87,84 +105,109 @@ class AccountInfoChange extends MenuNavigatorPage {
 }
 
 class _AccountInfoChangeState extends State<AccountInfoChange> {
+  final _formKey = GlobalKey<FormState>();
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  
-  @override
-  Widget build(BuildContext context) {
-    final user = _auth.currentUser;
-    String emailId = '';
+  String emailId = '';
+  String passwordDisplay = '';
 
+
+
+  @override
+  void initState() {
+    final user = _auth.currentUser;
     if (user != null) {
       emailId = user.email ?? '';
     }
+    super.initState();
+  }
 
+  @override
+  Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> usersStream =
+        FirebaseFirestore.instance.collection('users').snapshots();
     Widget emailDisplay = Text(emailId);
-    _emailController.text = emailId;
-    Widget passwordDisplay = getUserPassword(emailId);
 
-    return Column(children: [
-      const Icon(Icons.account_circle, size: 100),
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 64, vertical: 8),
-        child: TextFormField(
-          initialValue: '${(emailDisplay as Text).data}',
-          decoration: const InputDecoration(
-            border: UnderlineInputBorder(),
-            labelText: 'E-mail',
-          ),
-          onChanged: (name) {
-            setState(() {
-              _emailController.text = name;
-            });
+    return StreamBuilder(
+        stream: usersStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          final user = _auth.currentUser;
+
+          final userDoc =
+              snapshot.data!.docs.firstWhere((doc) => doc.id == emailId);
+          final data = userDoc.data() as Map<String, dynamic>;
+
+          if (snapshot.hasData) {
+            return Column(children: [
+              const Icon(Icons.account_circle, size: 100),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 64, vertical: 8),
+                child: TextFormField(
+                    initialValue: '${(emailDisplay as Text).data}',
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'E-mail',
+                    ),
+                    onChanged: (name) {
+                      setState(() {
+                        _emailController.text = name;
+                      });
+                    }),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 64, vertical: 8),
+                child: TextFormField(
+                    initialValue: data['password'],
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: 'Senha',
+                    ),
+                    onChanged: (senha) {
+                      setState(() {
+                        _passwordController.text = senha;
+                      });
+                    }),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 64, vertical: 8),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Conta Privada',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              BtnFilled(
+                  text: "Voltar",
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/accountInfoView');
+                  },
+                  backgroundColor: Colors.white,
+                  textColor: Theme.of(context).primaryColor),
+              const SizedBox(height: 30),
+              BtnFilled(
+                  text: "Confirmar",
+                  onPressed: () {
+                    if (user != null) {
+                      FirebaseFirestore.instance
+                          .collection('users')
+                          .doc(emailId)
+                          .update({'password': _passwordController.text});
+                      user.updatePassword(_passwordController.text);
+                    }
+                    Navigator.pushNamed(context, '/accountInfoView');
+                  },
+                  backgroundColor: Colors.white,
+                  textColor: Theme.of(context).primaryColor)
+            ]);
           }
-        ),
-      ),
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 64, vertical: 8),
-        child: TextFormField(
-          //initialValue: getUserPasswordString(emailId),
-          decoration: const InputDecoration(
-            border: UnderlineInputBorder(),
-            labelText: 'Senha',
-          ),
-          onChanged: (senha) {
-            setState(() {
-              _passwordController.text = senha;
-            });
-          }
-        ),
-      ),
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 64, vertical: 8),
-        child: TextFormField(
-          decoration: const InputDecoration(
-            border: UnderlineInputBorder(),
-            labelText: 'Conta Privada',
-          ),
-        ),
-      ),
-      const SizedBox(height: 30),
-      BtnFilled(
-          text: "Voltar",
-          onPressed: () {
-            Navigator.pushNamed(context, '/accountInfoView');
-          },
-          backgroundColor: Colors.white,
-          textColor: Theme.of(context).primaryColor),
-      const SizedBox(height: 30),
-      BtnFilled(
-          text: "Confirmar",
-          onPressed: () {
-            if (user != null) {
-              FirebaseFirestore.instance.collection('users').doc(emailId).update({'password': _passwordController.text});
-              user.updatePassword(_passwordController.text);
-            }
-            Navigator.pushNamed(context, '/accountInfoView');
-          },
-          backgroundColor: Colors.white,
-          textColor: Theme.of(context).primaryColor)
-    ]);
+
+          return const Text("Loading");
+        });
   }
 }
--- fitness_buddy/lib/pages/profile/personal_info/personal_info_page.dart
+++ fitness_buddy/lib/pages/profile/personal_info/personal_info_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fitness_buddy/pages/profile/menu/menu_navigator_page.dart';
 import 'package:fitness_buddy/pages/profile/menu/menu_navigator_view.dart';
@@ -11,60 +12,80 @@ class PersonalInfoView extends MenuNavigatorPage {
   State<PersonalInfoView> createState() => _PersonalInfoViewState();
 }
 
-
 class _PersonalInfoViewState extends State<PersonalInfoView> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  String emailId = '';
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
     final user = _auth.currentUser;
-    String emailId = '';
-
     if (user != null) {
       emailId = user.email ?? '';
     }
+    super.initState();
+  }
 
-    Widget nameDisplay = getUserName(emailId);
-    Widget birthDisplay = getUserBirth(emailId);
-    Widget heightDisplay = getUserHeight(emailId);
+  @override
+  Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> usersStream =
+        FirebaseFirestore.instance.collection('users').snapshots();
 
-    return Column(children: [
-      DisplayCard(
-        text: nameDisplay,
-        subtitle: "Nome:",
-        showIcon: false,
-      ),
-      DisplayCard(
-        text: birthDisplay,
-        subtitle: "Data de Nascimento:",
-        showIcon: false,
-      ),
-      DisplayCard(
-        text: heightDisplay,
-        subtitle: "Altura:",
-        showIcon: false,
-      ),
-      const SizedBox(height: 30),
-      BtnFilled(
-        text: "Alterar dados",
-        onPressed: () {
-          Navigator.pushNamed(context, '/personalInfoChange');
-        },
-        backgroundColor: Theme.of(context).primaryColor,
-        textColor: Colors.white,
-      ),
-      const SizedBox(height: 30),
-      BtnFilled(
-          text: "Voltar",
-          onPressed: () {
-            Navigator.pushNamed(context, '/');
-          },
-          backgroundColor: Colors.white,
-          textColor: Theme.of(context).primaryColor)
-    ]);
+    return StreamBuilder(
+        stream: usersStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          final userDoc =
+              snapshot.data!.docs.firstWhere((doc) => doc.id == emailId);
+          final data = userDoc.data() as Map<String, dynamic>;
+
+          Text displayBirth = const Text("Não informado.");
+          Text displayHeight = const Text("Não informado.");
+          if ((data['birth']) != null) {
+            displayBirth = data['birth'];
+          }
+          if ((data['height']) != null) {
+            displayHeight = data['height'];
+          }
+
+          if (snapshot.hasData) {
+            return Column(children: [
+              DisplayCard(
+                text: Text(data['name']),
+                subtitle: "Nome:",
+                showIcon: false,
+              ),
+              DisplayCard(
+                text: displayBirth,
+                subtitle: "Data de Nascimento:",
+                showIcon: false,
+              ),
+              DisplayCard(
+                text: displayHeight,
+                subtitle: "Altura:",
+                showIcon: false,
+              ),
+              const SizedBox(height: 30),
+              BtnFilled(
+                text: "Alterar dados",
+                onPressed: () {
+                  Navigator.pushNamed(context, '/personalInfoChange');
+                },
+                backgroundColor: Theme.of(context).primaryColor,
+                textColor: Colors.white,
+              ),
+              const SizedBox(height: 30),
+              BtnFilled(
+                  text: "Voltar",
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/');
+                  },
+                  backgroundColor: Colors.white,
+                  textColor: Theme.of(context).primaryColor)
+            ]);
+          }
+          return const Text("Loading");
+        });
   }
 }
-
 
 class PersonalInfoChange extends MenuNavigatorPage {
   const PersonalInfoChange({super.key});
@@ -74,46 +95,116 @@ class PersonalInfoChange extends MenuNavigatorPage {
 }
 
 class _PersonalInfoChangeState extends State<PersonalInfoChange> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _birthController = TextEditingController();
+  final TextEditingController _hieghtController = TextEditingController();
+  String emailId = '';
+
+  @override
+  void initState() {
+    final user = _auth.currentUser;
+    if (user != null) {
+      emailId = user.email ?? '';
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      const Icon(Icons.account_circle, size: 100),
-      Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 64, vertical: 8),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Nome',
-            ),
-          ),
-      ),
-      Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 64, vertical: 8),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Data de Nascimento',
-            ),
-          ),
-      ),
-      Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 64, vertical: 8),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Altura',
-            ),
-          ),
-      ),  
-      const SizedBox(height: 30),
-      BtnFilled(
-          text: "Voltar",
-          onPressed: () {
-            Navigator.pushNamed(context, '/personalInfoView');
-          },
-          backgroundColor: Colors.white,
-          textColor: Theme.of(context).primaryColor)
-    ]);
+    final Stream<QuerySnapshot> usersStream =
+        FirebaseFirestore.instance.collection('users').snapshots();
+
+    return StreamBuilder(
+        stream: usersStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+
+          final userDoc =
+              snapshot.data!.docs.firstWhere((doc) => doc.id == emailId);
+          final data = userDoc.data() as Map<String, dynamic>;
+
+          String? displayBirth;
+          String? displayHeight;
+          if ((data['birth']) != null) {
+            displayBirth = data['birth'];
+          }
+          if ((data['height']) != null) {
+            displayHeight = data['height'];
+          }
+
+          if (snapshot.hasData) {
+            return Column(children: [
+              const Icon(Icons.account_circle, size: 100),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 64, vertical: 8),
+                child: TextFormField(
+                  initialValue: data['name'],
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Nome',
+                  ),
+                  onChanged: (name) {
+                      setState(() {
+                        _nameController.text = name;
+                      });
+                  },
+                  validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor insira seu nome';
+                        }
+                        return null;
+                  }
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 64, vertical: 8),
+                child: TextFormField(
+                  initialValue: displayBirth,
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Data de Nascimento',
+                  ),
+                  onChanged: (birth) {
+                      setState(() {
+                        _birthController.text = birth;
+                      });
+                  },
+                  validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor insira seu nome';
+                        }
+                        return null;
+                  }
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 64, vertical: 8),
+                child: TextFormField(
+                  initialValue: displayHeight,
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Altura',
+                  ),
+                  onChanged: (height) {
+                      setState(() {
+                        _hieghtController.text = height;
+                      });
+                  }
+                ),
+              ),
+              const SizedBox(height: 30),
+              BtnFilled(
+                  text: "Voltar",
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/personalInfoView');
+                  },
+                  backgroundColor: Colors.white,
+                  textColor: Theme.of(context).primaryColor)
+            ]);
+          }return const Text("Loading");
+        });
   }
 }
