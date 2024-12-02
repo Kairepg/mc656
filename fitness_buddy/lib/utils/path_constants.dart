620ae8f502736d60b0afe2d67f1660a903408caf
Pedro Borges <p260628@dac.unicamp.br>
2024-12-02 18:57:18 -0300

    add constantes

---
+++ fitness_buddy/lib/utils/path_constants.dart
@@ -0,0 +1,48 @@
+class PathConstants {
+  // Onboarding
+  static const String onboarding1 = 'assets/images/onboarding/onboarding.png';
+  static const String onboarding2 = 'assets/images/onboarding/onboarding_2.png';
+  static const String onboarding3 = 'assets/images/onboarding/onboarding_3.png';
+
+  // Auth
+  static const String eye = 'assets/images/auth/eye_icon.png';
+
+  // Tabbar
+  static const String home = 'assets/icons/home/home_icon.png';
+  static const String workouts = 'assets/icons/home/workouts_icon.png';
+  static const String settings = 'assets/icons/home/settings_icon.png';
+
+  // Home
+  static const String profile = 'assets/images/home/profile.png';
+  static const String finished = 'assets/images/home/finished.png';
+  static const String inProgress = 'assets/icons/home/inProgress.png';
+  static const String timeSent = 'assets/icons/home/time.png';
+  static const String cardio = 'assets/images/home/cardio.png';
+  static const String arms = 'assets/images/home/arms.png';
+  static const String progress = 'assets/icons/home/progress.png';
+
+  // Workout Details
+  static const String yogaWorkout = 'assets/workouts_images/yoga_workout.png';
+  static const String back = 'assets/icons/workouts/back.png';
+  static const String rectangle = 'assets/icons/workouts/rectangle.png';
+  static const String timeTracker = 'assets/icons/workouts/time.png';
+  static const String exerciseTracker = 'assets/icons/workouts/exercise.png';
+
+  static const String recicling = 'assets/images/exercises/recicling.png';
+
+  //Workouts
+  static const String yoga = 'assets/workouts_images/yoga.png';
+  static const String pilates = 'assets/workouts_images/pilates.png';
+  static const String fullBody = 'assets/workouts_images/full_body.png';
+  static const String stretching = 'assets/workouts_images/stretching.png';
+
+  //social networks
+  static const String facebook = 'assets/icons/social_networks/facebook.png';
+  static const String instagram = 'assets/icons/social_networks/instagram.png';
+  static const String twitter = 'assets/icons/social_networks/twitter.png';
+
+  // Start workout -> Yoga
+  static const String recliningVideo = 'assets/videos/workouts/reclining.mp4';
+  static const String cowPoseVideo = 'assets/videos/workouts/cow.mp4';
+  static const String warriorIIVideo = 'assets/videos/workouts/warriorII.mp4';
+}
---
+++ fitness_buddy/lib/utils/text_constants.dart
@@ -0,0 +1,168 @@
+class TextConstants {
+  // Common
+  static const String start = "Start";
+
+  // Onboarding
+  static const String onboarding1Title = "Workout anywhere";
+  static const String onboarding2Title = "Learn techniques";
+  static const String onboarding3Title = "Stay strong & healthy";
+
+  static const String onboarding1Description = "You can do your workout at home without any equipment, outside or at the gym.";
+  static const String onboarding2Description = "Our workout programs are made by professionals.";
+  static const String onboarding3Description = "We want you to fully enjoy the program and stay healthy and positive.";
+
+  // Sign Up
+  static const String signUp = "Sign up";
+  static const String signIn = "Sign In";
+  static const String signOut = "Sign Out";
+
+  static const String username = "Username";
+  static const String userNamePlaceholder = "Your name";
+  static const String usernameErrorText = 'Text is required';
+
+  static const String email = "Email";
+  static const String emailPlaceholder = "example@mail.com";
+  static const String emailErrorText = 'Email is unvalid, please enter email properly';
+
+  static const String password = "Password";
+  static const String passwordPlaceholder = "Must be at least 6 symbols";
+  static const String passwordErrorText = 'Password should contain at least 6 characters';
+
+  static const String confirmPassword = "Confirm password";
+  static const String confirmPasswordPlaceholder = "Re-enter password";
+  static const String confirmPasswordErrorText = 'Password is not the same';
+
+  static const String alreadyHaveAccount = "Already have an account?";
+
+  // Sing In
+  static const String passwordPlaceholderSignIn = "Enter your password";
+  static const String forgotPassword = "Forgot password?";
+
+  static const String doNotHaveAnAccount = "Do not have an account?";
+
+  // Tab bar
+  static const String homeIcon = "Home";
+  static const String workoutsIcon = "Workouts";
+  static const String settingsIcon = "Settings";
+
+  // Home screen
+  static const String checkActivity = "Let's check your activity";
+  static const String finished = "Finished";
+  static const String completedWorkouts = "Completed workouts";
+
+  static const String inProgress = "In progress";
+  static const String workouts = "Workouts";
+
+  static const String timeSent = "Time sent";
+  static const String minutes = "Minutes";
+
+  static const String discoverWorkouts = "Discover new workouts";
+
+  static const String keepProgress = "Keep the progress!";
+  static const String profileSuccessful = "You are more successful than 88% users.";
+
+  // Home screen -> cardio card
+  static const String cardioTitle = "Cardio";
+  static const String cardioExercises = "10";
+  static const String cardioMinutes = "50";
+
+  // Home screen -> arms card
+  static const String armsTitle = "Arms";
+  static const String armsExercises = "6";
+  static const String armsMinutes = "35";
+
+  //Workout screen -> yoga card
+  static const String yogaTitle = "Yoga";
+  static const String yogaExercises = "16";
+  static const String yogaMinutes = "52";
+
+  //Workout screen -> pilates card
+  static const String pilatesTitle = "Pilates";
+  static const String pilatesExercises = "20";
+  static const String pilatesMinutes = "60";
+
+  //Workout screen -> fullBody card
+  static const String fullBodyTitle = "Full body";
+  static const String fullBodyExercises = "14";
+  static const String fullBodyMinutes = "48";
+
+  //Workout screen -> stretching card
+  static const String stretchingTitle = "Stretching";
+  static const String stretchingExercises = "8";
+  static const String stretchingMinutes = "35";
+
+  //Settings screen
+  static const String joinUs = 'Join us in social media';
+  static const String calendar = "Calendar";
+  static const String reminder = "Reminder";
+  static const String rateUsOn = "Rate us on ";
+  static const String terms = "Terms & Conditions";
+
+  // Workout Details screen common
+  static const String workout = "Workout";
+  static const String exercisesLowercase = "exercises";
+  static const String exercisesUppercase = "Exercises";
+
+  // Workout Details screen -> card1
+  static const String reclining = "Reclining to big toe";
+  static const int recliningMinutes = 12;
+
+  // Workout Details screen -> card2
+  static const String cowPose = "Cow Pose";
+  static const int cowPoseMinutes = 8;
+
+  // Workout Details screen -> card3
+  static const String warriorPose = "Warrior II Pose";
+  static const int warriorPoseMinutes = 12;
+
+  // Start Workout screen
+  static const String back = "Back";
+  static const String next = "Next";
+  static const String nextExercise = "Next Exercise:";
+
+  // Start Workout screen -> Warrior II Pose
+  static const String warriorDescription = "Named for a fierce warrior, an incarnation of Shiva, this version of Warrior Pose increases stamina.";
+  static const String warriorStep1 =
+      "Stand in Tadasana (Mountain Pose). Raise your arms parallel to the floor and reach them actively out to the sides, shoulder blades wide, palms down.";
+  static const String warriorStep2 =
+      "Turn your right foot slightly to the right and your left foot out to the left 90 degrees. Align the left heel with the right heel. Firm your thighs and turn your left thigh outward so that the center of the left knee cap is in line with the center of the left ankle.";
+
+  // Reminder screen
+  static const String selectTime = "Please select reminder time";
+  static const String save = "Save";
+  static const String repeating = "How often repeat";
+
+  // Reminder day of the week
+  static const String everyday = "Everyday";
+  static const String monday_friday = "Mon - Fri";
+  static const String weekends = "Weekends";
+  static const String monday = "Monday";
+  static const String tuesday = "Tue";
+  static const String wednesday = "Wed";
+  static const String thursday = "Thu";
+  static const String friday = "Fri";
+  static const String saturday = "Sat";
+  static const String sunday = "Sun";
+  // Edit account screen
+  static const String editAccount = 'Edit account';
+  static const String editPhoto = 'Edit photo';
+  static const String fullName = 'Full name';
+  static const String nameShouldContain2Char = 'Name should contain at least 2 characters';
+  static const String changePassword = 'Change Password';
+
+  // Edit account screen -> show settings
+  static const String cameraPermission = 'Camera permisson';
+  static const String cameAccess = 'This app needs camera access to take pictures for upload user profile photo';
+  static const String deny = 'Deny';
+  static const String settings = 'Settings';
+  static const String fullNamePlaceholder = 'Enter your full name';
+
+  //Change password screen
+  static const String newPassword = 'New password';
+  static const String passwordUpdated = 'Password successfully updated!';
+
+  //Forgot password screen
+  static const passwordReset = 'Password Reset';
+  static const sendActivationBuild = 'Send Activation Link';
+  static const resetPasswordLinkWasSent = 'Reset password link was sent on your email.';
+}
