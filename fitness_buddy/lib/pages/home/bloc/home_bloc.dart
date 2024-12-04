d0b4c3ea9160337826c4713880484136a2009a86
Guilherme <g260469@dac.unicamp.br>
2024-12-04 06:11:52 -0300

    Removendo unused import

--- fitness_buddy/lib/pages/home/bloc/home_bloc.dart
+++ fitness_buddy/lib/pages/home/bloc/home_bloc.dart
@@ -1,8 +1,8 @@
-import 'dart:async';
+// import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:fitness_buddy/data/workout_data.dart';
-import 'package:fitness_buddy/services/data_service.dart';
+// import 'package:fitness_buddy/services/data_service.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fitness_buddy/data/exercise_data.dart';
 // import 'package:fitness_buddy/pages/home/bloc/home_event.dart';
--- fitness_buddy/lib/pages/home/bloc/home_event.dart
+++ fitness_buddy/lib/pages/home/bloc/home_event.dart
@@ -5,7 +5,3 @@ part of 'home_bloc.dart';
 abstract class HomeEvent {}
 
 class HomeInitialEvent extends HomeEvent {}
-
-// class ReloadDisplayNameEvent extends HomeEvent {}
-
-// class ReloadImageEvent extends HomeEvent {}
